2723f774f81ecee67cf5ef79d5f7ff64d97a8e30
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-12-04 17:29:24 +0900

    deviseインストールテーブル設計

--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -13,6 +13,7 @@ class Recommend < ApplicationRecord
   validates :city_id,     numericality: { other_than: 1 , message: "can't be blank"}
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 
+  belongs_to :user
   has_one_attached :image
 
 end
