432c078f6baccce1df06fc9cf71e89e5d17a553e
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-12-05 19:39:52 +0900

    userテストコード終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: { maximum: 8 }
 
   has_many :recommends
   
