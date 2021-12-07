87e9fcab0700ce3bf6f46be70146f0d3cdcd9ddb
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-12-07 14:53:03 +0900

    表示サイズ、画面装飾完了

--- db/migrate/20211202182508_create_recommends.rb
+++ db/migrate/20211202182508_create_recommends.rb
@@ -1,7 +1,7 @@
 class CreateRecommends < ActiveRecord::Migration[6.0]
   def change
     create_table :recommends do |t|
-      t.string :place_name, null: false, unique: true
+      t.string :place_name, null: false
       t.integer :region_id, null: false
       t.integer :city_id, null: false
       t.integer :category_id, null: false
